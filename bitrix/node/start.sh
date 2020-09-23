bd532651d5069ebad02b48ac767156b18bad5e75
Dmitriy <ignashkins@yandex.ru>
2020-09-23 19:27:05 +0300

    Push&Pull

--- bitrix/node/start.sh
+++ bitrix/node/start.sh
@@ -1,14 +1,13 @@
 #!/bin/sh
 
+echo "FUCK YOU SPILBERG"
+
+
 /usr/bin/redis-server /etc/redis.conf
+#
+#sleep 2
+#
+#chown redis:bitrix /tmp/redis.sock
 
-sleep 2
 
-chown redis:bitrix /tmp/redis.sock
 
-ls -la /etc | grep redis
-ls -la /tmp
-
-/etc/init.d/push-server-multi systemd_start
-
-/bin/sh
